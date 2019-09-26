7bfd9c5a14ba509de337e919f0d5458b58abe0ae
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-26 21:29:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,15 @@
 def find_element_index(array, value_to_find)
  array.length.times { |index|
    if array[index] == value_to_find
-   return index
- end 
+     return index
+  end 
  }
  return nil
 end
   
 
-
 def find_max_value(array)
+  
 end
 
 def find_min_value(array)
